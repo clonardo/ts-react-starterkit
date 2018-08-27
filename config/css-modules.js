f32078da671cca0719259a288bea0a18d440e006
Giovanni Tramutola <giovanni.tramutola@contactlab.com>
2018-08-27 12:47:13 +0200

    refactor(css-modules.js): Fix createHash and cssModulesNameGenerator function

--- config/css-modules.js
+++ config/css-modules.js
@@ -4,12 +4,9 @@ function createHash () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-module.exports = function cssModulesNameGenerator (loaderContext, localIdentName, localName, options) {
+function cssModulesNameGenerator (loaderContext, localIdentName, localName, options) {
   const fileName = path.basename(loaderContext.resourcePath);
-  if (fileName.indexOf('.global.scss') !== -1) {
-    return localName;
-  } else {
-    const name = fileName.replace(/\.[^/.]+$/, '');
-    return `${localName}__${createHash()}`;
-  }
+  return fileName.indexOf('.global.scss') !== -1 ? localName : `${localName}__${createHash()}`;
 }
+
+module.exports = cssModulesNameGenerator;
