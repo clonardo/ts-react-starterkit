2dcea701f3c2d61c7914ba77c2b22ed4b5556cf2
Giovanni Tramutola <giovanni.tramutola@contactlab.com>
2018-08-27 10:01:21 +0200

    chore: Fix module export in css-modules

--- config/css-modules.js
+++ config/css-modules.js
@@ -2,7 +2,7 @@ function createHash () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-export default function cssModulesNameGenerator (loaderContext, localIdentName, localName, options) {
+module.exports = function cssModulesNameGenerator (loaderContext, localIdentName, localName, options) {
   const fileName = path.basename(loaderContext.resourcePath);
   if (fileName.indexOf('.global.scss') !== -1) {
     return localName;
